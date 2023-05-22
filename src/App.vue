e7364f3472a42777536897ceee9ec4337927c8f6
Andreaserra1997 <serra.andrea1997@gmail.com>
2023-05-22 14:48:46 +0200

    add components

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script>
+import AppSearch from "./components/AppSearch.vue";
+import CharacterList from "./components/CharacterList.vue";
+
+export default {
+  components: {
+    AppSearch,
+    CharacterList,
+  },
+};
+</script>
+
+<template>
+  <h1>Yu-Gi-Oh Api</h1>
+  <AppSearch />
+  <CharacterList />
+</template>
+
+<style lang="scss"></style>
---
+++ src/components/CharacterList.vue
@@ -0,0 +1,15 @@
+<script>
+import CharacterCard from "./CharacterCard.vue";
+
+export default {
+  components: {
+    CharacterCard,
+  },
+};
+</script>
+
+<template>
+  <CharacterCard />
+</template>
+
+<style lang="scss" scoped></style>
